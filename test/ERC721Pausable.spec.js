98524199ba9eea94d3ce8ed5b264e2a5f469fc10
Jesús Castro <x51v4n@gmail.com>
2021-01-20 12:51:40 -0600

    Feature: Add ERC721 Pausable tests
    1. Why is this change necessary?
    This commits adds the ER721 Pausable testing methods.
    
    2. How does it address the issue?
    By adding the test suite focusing on the Pausable contract.
    
    3. What side effects does this change have?
    None, it's the first approximation of this test suite.

--- test/ERC721Pausable.spec.js
+++ test/ERC721Pausable.spec.js
@@ -1,6 +1,21 @@
 const ERC721Mintable = artifacts.require('ERC721Mintable')
-const truffleAssert = require('truffle-assertions');
+//const truffleAssert = require('truffle-assertions');
 
 contract('Pausable', (accounts) => {
-    
+
+    const account_one = accounts[0]
+
+    describe('Pausable methods Test', () => {
+        beforeEach(async () => {
+            this.contract = await ERC721Pausable.new({ from: account_one });
+            await this.contract.mint(account_one, 1, { from: account_one});
+        })
+
+        it('should update status', async () => {
+            isPaused = false;
+            status = isPaused;
+            isPaused = await this.contract.updateStatus(status);
+            assert.equal(isPaused, true, 'Must update status');
+        })
+    });
 })
